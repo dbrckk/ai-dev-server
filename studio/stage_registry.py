dbb99645f03e0daecb7a129fa7e86fd1c6428494
dbrckk <131650085+dbrckk@users.noreply.github.com>
2026-09-08 20:06:41 +0200

    Register notification QA stage

--- studio/stage_registry.py
+++ studio/stage_registry.py
@@ -17,6 +17,7 @@ STAGES = {
     'capability_qa': Stage('capability_qa', 'studio/capability_stage.py', 'deferred_capability', 'capability_failed'),
     'performance_qa': Stage('performance_qa', 'studio/performance_stage.py', 'deferred_performance', 'performance_failed'),
     'native_qa': Stage('native_qa', 'studio/native_stage.py', 'deferred_native', 'native_failed'),
+    'notification_qa': Stage('notification_qa', 'studio/notification_stage.py', 'deferred_notification', 'notification_failed'),
     'store_metadata': Stage('store_metadata', 'studio/store_stage.py', 'deferred_store', 'store_failed'),
     'privacy_policy': Stage('privacy_policy', 'studio/privacy_stage.py', 'deferred_privacy', 'privacy_failed'),
     'security_scan': Stage('security_scan', 'studio/security_stage.py', 'deferred_security', 'security_failed'),
